77bdfef702a2b19c2eb9686efe2302eab0f5b834
seregapanteleev <seregapanteleev@mail.ru>
2021-01-16 20:22:27 +0300

    Update Main.java

--- academy/src/by/academy/lesson2/Main.java
+++ academy/src/by/academy/lesson2/Main.java
@@ -2,7 +2,8 @@ package by.academy.lesson2;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Second commit");
+		
+		System.out.println("Second commit new");
 
 	}
 
