9e4f2763324aa15a1cb34e57cb59063fbe53f501
seregapanteleev <seregapanteleev@mail.ru>
2021-01-30 22:51:47 +0300

    Task 0

---
+++ academy/src/by/academy/homework/homework2/Solution.java
@@ -0,0 +1,39 @@
+package by.academy.homework.homework2;
+
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	static int pairs(int k, int[] arr) {
+		int pairsCount = 0;
+		int a = 0;
+		for (int i = 0; i < arr.length; i++) {
+			a++;
+			for (int j = a; j < arr.length; j++) {
+				if (Math.abs(arr[i] - arr[j]) == k) {
+					pairsCount++;
+				}
+			}
+		}
+		return pairsCount;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+
+		String[] nk = scanner.nextLine().split(" ");
+		int n = Integer.parseInt(nk[0]);
+		int k = Integer.parseInt(nk[1]);
+		int[] arr = new int[n];
+		String[] arrItems = scanner.nextLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(arrItems[i]);
+		}
+		int result = pairs(k, arr);
+		System.out.println(result);
+		scanner.close();
+	}
+}
